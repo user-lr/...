320edf488dda0b4ad4ea0c20f8b705322243b932
user-lr <2793094623@qq.com>
2024-11-15 11:44:28 +0800

    Update indexl.js

--- js/indexl.js
+++ js/indexl.js
@@ -18,7 +18,7 @@ var S = {
             m++;
       S.Shape.render();
       //console.log(m);
-      if(m==2500){
+      if(m==2000){
         window.location.href="../html/BirthdayCake.html";
       }
     });
